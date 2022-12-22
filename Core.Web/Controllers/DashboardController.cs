6b0f71e77e8812f05463b180861b946e2762e267
njorogesamuel41 <njorogesamuel41@yahoo.com>
2022-12-22 18:07:16 +0300

    Add cart menu

--- Core.Web/Controllers/DashboardController.cs
+++ Core.Web/Controllers/DashboardController.cs
@@ -36,5 +36,35 @@ namespace Core.Web.Controllers
 
             return this.DataTablesJson(new List<ProductDTO>(), totalRecordCount, searchRecordCount, jQueryDataTablesModel.sEcho);
         }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cart(JQueryDataTablesModel jQueryDataTablesModel)
+        {
+            int totalRecordCount = 0;
+
+            int searchRecordCount = 0;
+
+            //  var sortedColumns = (from s in jQueryDataTablesModel.GetSortedColumns() select s.PropertyName).ToList();
+
+            //  sortedColumns.Add("CreatedDate");
+            //ToDo: make API call to get list of products
+            var pageCollectionInfo = new PageCollectionInfo<ProductDTO>();
+
+            if (pageCollectionInfo != null && pageCollectionInfo.PageCollection.Any())
+            {
+                totalRecordCount = pageCollectionInfo.ItemsCount;
+
+                searchRecordCount = !string.IsNullOrWhiteSpace(jQueryDataTablesModel.sSearch) ? pageCollectionInfo.PageCollection.Count : totalRecordCount;
+
+                return this.DataTablesJson(pageCollectionInfo.PageCollection, totalRecordCount, searchRecordCount, jQueryDataTablesModel.sEcho);
+            }
+
+            return this.DataTablesJson(new List<ProductDTO>(), totalRecordCount, searchRecordCount, jQueryDataTablesModel.sEcho);
+        }
     }
 }
